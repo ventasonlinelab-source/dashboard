9da5ceff1c6476ec4935bec6dff39db73726f8e2
ventasonlinelab-source <ventasonlinelab@gmail.com>
2026-06-30 11:34:48 +0200

    Update update-kpi.js

--- api/update-kpi.js
+++ api/update-kpi.js
@@ -1,15 +1,11 @@
+import { Redis } from '@upstash/redis';
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
-
-  // Esta URL ya contiene las credenciales
-  const url = `${process.env.REDIS_URL}/set/kpi_data`;
   
   try {
-    const response = await fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(req.body),
-    });
-
+    const redis = Redis.fromEnv();
+    await redis.set('kpi_data', req.body);
     return res.status(200).json({ status: "Guardado" });
   } catch (error) {
     return res.status(500).json({ error: error.message });
